02d3be150eb6f3e0b015d1b26fe62201064885bb
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-02-10 11:45:19 +0100

    cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Task.create(name: 'Convinient store', description: 'Pick up the grocery list and pay all the items on it', importance: 3)
+Task.create(name: 'Pay the taxes', description: 'Check the accuracy and send a check', importance: 1)
+Task.create(name: 'Mail your friend', description: 'Send an email to Max', importance: 2)
